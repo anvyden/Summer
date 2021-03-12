0beaef09c53176d185fb302e62d2983e70ef79d6
anvyden <61583979+anvyden@users.noreply.github.com>
2021-03-12 12:08:34 +0700

    add modal window and make buttons modal functional

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
+  let modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+
+// Функционал модального окна
+  modalBtn.on('click', function() {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function() {
+    modal.toggleClass('modal--visible');
+  });
+  modal.on('сlick', function(e) {
+    if ($(e.target).is('modal__dialog'))
+    e.stopPropagation();
+    else if ($(e.target).is('modal'))
+    modal.removeClass('modal--visible');
+  });
+  $(document).on('keydown', function(e) {
+    if (e.keyCode === 27 && $('.modal').hasClass('modal--visible'))
+    modal.toggleClass('modal--visible');
+  });
+
+// Инициализация слайдеров
   const swiper = new Swiper('.swiper-container-hero', {
     autoplay: {
       delay: 5000,
